8830f57d01c55a2af88af691372a8ce903ed927e
Rob Nelson <guruvan@maza.club>
2018-01-11 11:30:49 -0800

    make mazad branch for commits and builds

--- letsencrypt/README.md
+++
@@ -1,7 +0,0 @@
-# Obtaining SSL certs via Letsencrypt
-
-This is simplest with the builtin webserver in the 
-
-certbot/certbot docker image
-
-mazanode/scripts/get_cert.sh 
--- mongodb/create_iquidus_indexes.sh
+++
@@ -1,4 +0,0 @@
-sh -c 'mongo iquidus -u iquidus -p 123dontgetme --eval  "db.addresses.createIndex( { a_id : 1})"' -s /bin/ash mongodb
-sh -c 'mongo iquidus -u iquidus -p 123dontgetme --eval  "db.txes.createIndex( { txid : 1})"' -s /bin/ash mongodb
-
- sh -c 'mongo iquidus -u iquidus -p 123dontgetme --eval  "db.txes.createIndex({timestamp: -1})"'  -s /bin/ash mongodb
